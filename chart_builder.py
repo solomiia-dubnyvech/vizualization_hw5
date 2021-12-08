2efde8a5a7315fa65f4085e600364264d8cc8e2b
solomiia-dubnyvech <dubnevych@ucu.edu.ua>
2021-12-08 21:46:28 +0200

    homework 6 done. added interactiveness

--- chart_builder.py
+++ chart_builder.py
@@ -2,27 +2,31 @@ import altair as alt
 import pandas as pd
 
 
+def get_selection():
+    return alt.selection_single(fields=['region'], on='mouseover', empty='all')
+
+
 def build_background(country):
     return alt.Chart(country, height=300, width=600).project().mark_geoshape(fill='lightgray', stroke='white')
 
 
-def build_map(world, data, lookup_field, scale, field_type='Q'):
+def build_map(world, data, selection):
     return alt.Chart(world, height=300, width=600).project().mark_geoshape(stroke='white').encode(
-        color=alt.Color(f'{lookup_field}:{field_type}', scale=scale),
-        tooltip=[alt.Tooltip('region:N'), alt.Tooltip(f'{lookup_field}:{field_type}')],
+        color=alt.Color('rate:Q', scale=alt.Scale(scheme='orangered', reverse=True)),
+        tooltip=[alt.Tooltip('region:N'), alt.Tooltip('rate:Q')],
+        opacity=alt.condition(selection, alt.value(1), alt.value(0.1))
     ).transform_lookup(
         lookup='region',
-        from_=alt.LookupData(data=data, key='region', fields=['region', lookup_field]),
-    )
+        from_=alt.LookupData(data=data, key='region', fields=['region', 'rate']),
+    ).add_selection(selection)
 
 
-def build_lines(data):
-    selection = alt.selection_multi(fields=['region'], bind='legend')
+def build_lines(data, selection):
     return alt.Chart(data, height=300, width=600).mark_line().encode(
         x=alt.X('year:O'),
         y=alt.Y('rate:Q'),
-        color=alt.Color('region:N', scale=alt.Scale(scheme='category20')),
-        opacity=alt.condition(selection, alt.value(1), alt.value(0.1))
+        color=alt.condition(selection, 'region:N', alt.value('gray')),
+        size=alt.condition(selection, alt.value(2), alt.value(0.2)),
     ).add_selection(selection)
 
 
--- main.py
+++ main.py
@@ -3,26 +3,24 @@ from source import *
 from params import *
 
 
-def task1():
+def task1(selection):
     country_map = load_map()
     agg_data = load_data()
     background = build_background(country_map)
-    chart = build_map(world=country_map,
-                      data=agg_data[agg_data['year'] == 2019],
-                      lookup_field='rate',
-                      scale=alt.Scale(scheme='orangered', reverse=True))
+    chart = build_map(country_map, agg_data[agg_data['year'] == 2019], selection)
 
     return background + chart + build_description()
 
 
-def task2():
+def task2(selection):
     agg_data = load_data()
-    chart = build_lines(agg_data)
+    chart = build_lines(agg_data[agg_data['year'] >= 2000], selection)
 
     return chart
 
 
 if __name__ == '__main__':
     alt.data_transformers.disable_max_rows()
-    add_title(chart=task1() & task2(),
+    hover_selection = get_selection()
+    add_title(chart=task1(hover_selection) & task2(hover_selection),
               title='Рівень приросту/скорочення за регіонами').show()
